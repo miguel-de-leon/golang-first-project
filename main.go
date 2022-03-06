88756eb482df64c94dea8b2cf9fac9b1ed6f015f
Miguel de Leon <miguelin.er09@gmail.com>
2022-03-06 12:38:49 -0600

    Refresh a bout GoLang Syntax

--- main.go
+++ main.go
@@ -1,3 +1,36 @@
 package main
 
-func main()
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var x int
+	x = 8
+	y := 7
+	fmt.Println(x)
+	fmt.Println(y)
+
+	myValue, err := strconv.ParseInt("asdf", 0, 64)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	} else {
+		fmt.Println(myValue)
+	}
+
+	m := make(map[string]int)
+	m["Key"] = 6
+	fmt.Println(m["Key"])
+
+	s := []int{1, 2, 3}
+	for index, value := range s {
+		fmt.Println(index, value)
+	}
+
+	s = append(s, 16)
+
+	for index, value := range s {
+		fmt.Println(index, value)
+	}
+}
